a07b93f6e3171bd5325230e9de6933608fc60a87
Franco <frfantusatti@protonmail.com>
2026-06-22 08:02:16 -0300

    fixed end if

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from .models import Workout
 
+
 class WorkoutForm(forms.ModelForm):
 
     class Meta:
         model = Workout
         fields = ['activity', 'duration', 'date']
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'activity': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'e.g. Running, Cycling, Yoga…',
+            }),
+            'duration': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'placeholder': '30',
+                'min': '1',
+            }),
+            'date': forms.DateInput(attrs={
+                'class': 'form-control',
+                'type': 'date',
+            }),
         }
